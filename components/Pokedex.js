becab4df5112e9aebf97bace59feaa814ba4abc9
vn50j6n <Zev.Yirmiyahu@walmart.com>
2019-12-04 01:05:43 -0500

    state management with redux working, list of pokemon managed with redux

--- components/Pokedex.js
+++ components/Pokedex.js
@@ -9,7 +9,6 @@ export const getPokemon = (id) => {
     fetch(URL)
         .then(response => response.json())
         .then(responseJson => {
-            console.log(responseJson);
             return responseJson;
         }).catch(error => console.log(error)); // catches errors if any
 }
@@ -24,43 +23,24 @@ class Pokedex extends Component {
         pokemonList: []
     }
 
+    getRandomPokemon = () => {
+        this.props.dispatch({ type: 'RANDOM_POKEMON' })
+    }
+    
     addToPokemonList = () => {
         this.props.dispatch({ type: 'STORE_POKEMON' });
     }
-
-
-    // GET request for a random pokemon
-    getRandomPokemon = () => {
-        let id = Math.floor(Math.random() * 100); // random number from 0 - 99
-        let URL = 'https://pokeapi.co/api/v2/pokemon/' + id;
-        let pokemon = {
-            name: '',
-            id: id,
-            image: '',
-        }
-        fetch(URL)
-            .then(response => response.json())
-            .then(responseJson => {
-                pokemon.name = responseJson.name;
-                pokemon.image = '';
-                state.pokemon = pokemon;
-                this.addToPokemonList();
-            }).catch(error => console.log(error)); // catches errors if any
-            console.log(this.state.pokemon);
-    }
-
+    
     render() {
         return (
             <View>
                 <Text> Pokedex </Text>
-                {/* <Button title='Random Pokemon' onPress={() => {this.props.dispatch({type: 'RANDOM_POKEMON'})}} /> */}
                 <Button title='Random Pokemon' 
                     onPress={() => { 
-                        let pokemon = this.getRandomPokemon(); 
+                        this.getRandomPokemon(); 
+                        this.addToPokemonList();
                         console.log(this.props.pokemonList); 
                     }} />
-
-                {/* <Text>Name: {this.props.pokemon}</Text> */}
             </View>
         );
     }
--- store/Store.js
+++ store/Store.js
@@ -11,11 +11,29 @@ const initialState = {
 }
 
 storePokemon = (state = initialState, action) => {
-    switch(action.type) {
+    switch (action.type) {
+        case 'RANDOM_POKEMON':
+            let id = Math.floor(Math.random() * 100); // random number from 0 - 99
+            let URL = 'https://pokeapi.co/api/v2/pokemon/' + id;
+            let pokemon = {
+                name: '',
+                id: id,
+                image: '',
+            }
+            fetch(URL)
+                .then(response => response.json())
+                .then(responseJson => {
+                    pokemon.name = responseJson.name;
+                    pokemon.image = '';
+                }).catch(error => console.log(error)); // catches errors if any
+                return {
+                    pokemon: pokemon,
+                    pokemonList: state.pokemonList
+                }
         case 'STORE_POKEMON':
             return {
                 pokemon: state.pokemon,
-                pokemonList: state.pokemonList.push(state.pokemon)
+                pokemonList: [...state.pokemonList, state.pokemon]
             }
         default: return state;
     }
