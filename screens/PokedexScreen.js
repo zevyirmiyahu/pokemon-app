0bf244e9c9cd886051451fc0ef7199dd90d261f0
vn50j6n <Zev.Yirmiyahu@walmart.com>
2019-12-31 15:49:23 -0500

    issue fixed with provider, pokedex now runs again as intended

--- screens/PokedexScreen.js
+++ screens/PokedexScreen.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
 
+import store from '../store/Store';
 import Pokedex from '../components/Pokedex';
 
 export default class PokedexScreen extends Component {
 
     render() {
         return (
-            <View>
-                <Pokedex />
-            </View>
+            <Provider store={store}>
+                <View>
+                    <Pokedex />
+                </View>
+            </Provider>
         );
     }
 }
